4be6bf022c58bd75f55f061555dd035e3373a247
伊藤直哉 <n_itou@itounaoya-no-MacBook-Air.local>
2016-01-15 10:15:28 +0900

    fix api url

--- app/models/article.rb
+++ app/models/article.rb
@@ -5,8 +5,7 @@ class Article < ActiveRecord::Base
 
   class << self
     def new_post
-      #url =  URI.parse('https://blog.hatena.ne.jp/siki_kawa/kawa-e.hateblo.jp/atom/entry')
-      url = 'https://blog.hatena.ne.jp/siki_kawa/kawa-e.hateblo.jp/atom'
+      url = 'https://blog.hatena.ne.jp/siki_kawa/kawa-e.hateblo.jp/atom/entry'
 
       # WSSE authentication
       user = 'siki_kawa'
@@ -29,37 +28,6 @@ ENDOFCONENT
       res = client.create_entry(url, entry);
 
       Article.create(title: "now #{Time.now}", body: res)
-=begin
-      header = {
-        "X-WSSE" => self.get_wsse(user, api_key),
-        "Accept" => "application/x.atom+xml, application/xml, text/xml, */*"
-      }
-      uri = 'http://yahoo.co.jp/'
-      tags = "test"
-      xml = "<entry xmlns=\"http://purl.org/atom/ns#\">
-      <title>dummy</title>
-      <link rel=\"related\" type=\"text/html\" href=\"#{uri}\" />
-      <summary type=\"text/plain\">#{tags}
-      </summary></entry>"
-
-      response = Net::HTTP.start(api.hostname, api.port) do |http|
-        http.post(api, xml, header)
-      end
-      xml = Nokogiri::XML(response.body)
-=end
-    end
-
-    def get_wsse(user, api_key)
-      created = Time.now.iso8601
-      nonce = ''
-      20.times do 
-        nonce << rand(256).chr
-      end
-      passdigest = Digest::SHA1.digest(nonce + created + api_key)
-      return "UsernameToken Username=\"#{user}\", " +
-             "PasswordDigest=\"#{Base64.encode64(passdigest).chomp}\", " + 
-             "Nonce=\"#{Base64.encode64(nonce).chomp}\", " +
-             "Created=\"#{created}\""
     end
   end
 end
