9aa8282062c1626342d7103dcb3a0df54ef59b9d
伊藤直哉 <n_itou@itounaoya-no-MacBook-Air.local>
2016-01-24 10:40:37 +0900

    fix bug

--- app/models/article.rb
+++ app/models/article.rb
@@ -6,7 +6,7 @@ require 'trollop'
 class Article < ActiveRecord::Base
   scope :recent, -> { order('id DESC') }
   scope :success, -> { where("failed_flag != ?", false) }
-  scope :active, -> { where("deleted_at != ?", true) }
+  scope :active, -> { where("deleted_at IS NULL") }
 
   YOUTUBE_DEVELOPER_KEY = 'AIzaSyAzoxN3WVjJ-Oa1eu0BontCw-G8W15MyuM'
   YOUTUBE_API_SERVICE_NAME = 'youtube'
