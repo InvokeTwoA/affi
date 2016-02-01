0b346bb7899d11cde6d26a04c550ccbccf03daaa
伊藤直哉 <n_itou@itounaoya-no-MacBook-Air.local>
2016-02-01 18:53:49 +0900

    fix bug

--- app/models/ng_word.rb
+++ app/models/ng_word.rb
@@ -4,7 +4,7 @@ class NgWord < ActiveRecord::Base
   class << self
     def is_ok?(title)
       NgWord.recent.each do |ng_word|
-        if title.include?(ng_word)
+        if title.include?(ng_word.name)
           ng_word.hits_count += 1
           ng_word.save!
           return false
