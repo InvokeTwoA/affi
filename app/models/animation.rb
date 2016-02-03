251f630a01ab8bb8fb5f38b8532788a9aceed0f6
伊藤直哉 <n_itou@itounaoya-no-MacBook-Air.local>
2016-02-04 02:42:17 +0900

    はてぶ投稿が失敗しても処理が続くよう修正

--- app/models/animation.rb
+++ app/models/animation.rb
@@ -54,4 +54,20 @@ class Animation < ActiveRecord::Base
       Hatena.update_blog(user, api_key, url, title, body)
     end
   end
+
+  class << self
+    def update_article
+      # 無料アニメを追加（ただし、更新日がマッチしたもののみ）
+      wdays = ["日", "月", "火", "水", "木", "金", "土"]
+      youbi = wdays[Time.now.wday]
+      hour =  Time.now.hour
+      Animation.recent.each do |animation|
+        next unless youbi == animation.onair_youbi
+        next unless hour == animation.onair_hour
+        animation.story_no += 1
+        animation.save!
+        animation.post_article
+      end
+    end
+  end
 end
--- lib/tasks/articles.rake
+++ lib/tasks/articles.rake
@@ -1,18 +1,7 @@
 namespace :articles do
   desc '新しい記事入稿'
   task new_post: :environment do
-    # 無料アニメを追加（ただし、更新日がマッチしたもののみ）
-    wdays = ["日", "月", "火", "水", "木", "金", "土"]
-    youbi = wdays[Time.now.wday]
-    hour =  Time.now.hour
-    Animation.recent.each do |animation|
-      next unless youbi == animation.onair_youbi
-      next unless hour == animation.onair_hour
-      animation.story_no += 1
-      animation.save!
-      animation.post_article
-    end
-
+    Animate.update_article
     begin
       # グラビア関係の記事を追加
       Article.new_post('idol')
